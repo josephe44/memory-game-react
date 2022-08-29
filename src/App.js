301438ee15358744847ab660cd052999f7ea1bfe
Osuohia Emmanuel <josephe442@gmail.com>
2022-08-29 23:58:04 +0100

    disabling card selections

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
 
   // shuffle cards
   // TODO: 1 - take the card array and duplicate them into 12
@@ -37,6 +38,7 @@ function App() {
   // Compare 2 selected card
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards((prevCard) => {
           return prevCard.map((card) => {
@@ -61,6 +63,7 @@ function App() {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns((prev) => prev + 1);
+    setDisabled(false);
   };
 
   return (
@@ -75,6 +78,7 @@ function App() {
             card={card}
             handleChoice={handleChoice}
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
